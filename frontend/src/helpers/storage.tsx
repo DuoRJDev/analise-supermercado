f6220519f79fb7e1a6f00cc21832cc0e62967b37
Yago Calomino <yagoferrocalomino@gmail.com>
2023-09-26 13:16:08 -0300

    Começando Login

---
+++ frontend/src/helpers/storage.tsx
@@ -0,0 +1,5 @@
+// export const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
+
+// export const saveLocalStorage = (key, object) => {
+//     localStorage.setItem(key, JSON.stringify(object));
+// };
