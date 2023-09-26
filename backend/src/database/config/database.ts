11481ef4b87c0a905700dce409babacd4a300ac4
joblanco <jo_blanco9@yahoo.com.br>
2023-09-26 14:06:09 -0300

    Criada Models do Banco de Dados

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -3,7 +3,7 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '123456',
-  database: 'ANALISE_SUPERMERCADO_DUORJDEV',
+  database: process.env.DATABASE || 'ANALISE_SUPERMERCADO_DUORJDEV',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
---
+++ backend/src/database/models/Regions.ts
@@ -0,0 +1,25 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class Regions extends Model<InferAttributes<Regions>,
+  InferCreationAttributes<Regions>> {
+  declare id: CreationOptional<number>;
+  declare region: CreationOptional<string>;
+}
+
+Regions.init({
+  id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+  region: { type: DataTypes.STRING, allowNull: false },
+}, {
+  sequelize: db,
+  timestamps: false,
+  underscored: false,
+});
+
+export default Regions;
