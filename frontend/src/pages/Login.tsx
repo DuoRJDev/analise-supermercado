d6205ee56ec8afee98771d8ac4d8acbc68a60bda
joblanco <jo_blanco9@yahoo.com.br>
2023-10-07 18:04:53 -0300

    Refatoração

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import React, { useState, type ChangeEvent, type FormEvent } from 'react';
+import { saveLocalStorage } from '../helpers/storage';
 import axios from 'axios';
 
 function Login() {
@@ -6,28 +7,23 @@ function Login() {
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
+  const URL_LOGIN = 'http://localhost:3001/login';
 
-  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    const newEmail = e.target.value;
-    setEmail(newEmail);
-    validateEmail(newEmail);
-  };
-
-  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    const newPassword = e.target.value;
-    setPassword(newPassword);
-    validatePassword(newPassword);
-  };
-
-  const validateEmail = (newEmail: string): void => {
+  const validateEmail = (emailInput: string): void => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const isValidEmail = emailRegex.test(newEmail);
+    const isValidEmail = emailRegex.test(emailInput);
 
-    if (!isValidEmail && newEmail.length > 0) {
+    if (!isValidEmail && emailInput.length > 0) {
       setEmailError('E-mail inválido. Certifique-se de que contém "@" e ".com".');
     } else {
       setEmailError('');
     }
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const emailInput = e.target.value;
+    setEmail(emailInput);
+    validateEmail(emailInput);
   };
 
   const validatePassword = (newPassword: string): void => {
@@ -41,61 +37,73 @@ function Login() {
     }
   };
 
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+    validatePassword(newPassword);
+  };
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    if (!emailError && !passwordError) {
-      try {
-        const response = await axios.post('url-de-login', {
-          email,
-          password
-        });
+    try {
+      const response = await axios.post(URL_LOGIN, {
+        email,
+        password
+      });
+      // Validação de token da resposta necessária e adição do mesmo no header para manter login
 
-        if (response.data.success) {
-          alert('Login bem-sucedido!');
-        } else {
-          alert('Login falhou. Verifique suas credenciais.');
-        }
-      } catch (error) {
-        console.error('Erro ao fazer login:', error);
-        alert('Ocorreu um erro ao fazer login. Tente novamente mais tarde.');
-      }
-
-      localStorage.setItem('user', JSON.stringify({ email }));
+      // if (response.data.success) {
+      //   alert('Login bem-sucedido!');
+      // } else {
+      //   alert('Login falhou. Verifique suas credenciais.');
+      // }
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+      alert('Ocorreu um erro ao fazer login. Tente novamente mais tarde.');
     }
+    saveLocalStorage('user', email);
+    // localStorage.setItem('user', JSON.stringify({ email }));
   };
 
   return (
     <div>
       <h2>Tela de Login</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">E-mail:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={handleEmailChange}
-          data-testid="email-input"
-          required
-        />
-        <br />
-        {emailError && <p style={{ color: 'red' }}>{emailError}</p>}
-        <label htmlFor="password">Senha:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={handlePasswordChange}
-          data-testid="password-input"
-          required
-        />
-        <br />
-        {passwordError && <p style={{ color: 'red' }}>{passwordError}</p>}
-        <button type="submit" data-testid="login-submit-btn" disabled={!!emailError || !!passwordError}>
-          Entrar
-        </button>
-      </form>
+      <label htmlFor="email">E-mail:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={email}
+        onChange={handleEmailChange}
+        data-testid="email-input"
+        required
+      />
+      <br />
+      {emailError && <p style={{ color: 'red' }}>{emailError}</p>}
+      <label htmlFor="password">Senha:</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        value={password}
+        onChange={handlePasswordChange}
+        data-testid="password-input"
+        required
+      />
+      <br />
+      {passwordError && <p style={{ color: 'red' }}>{passwordError}</p>}
+      <button
+        onClick={handleSubmit}
+        data-testid="login-submit-btn"
+        disabled={!!emailError || !!passwordError}
+      >
+        Login
+      </button>
+      <button
+        data-testid="login-submit-btn"
+      >
+        Criar nova conta
+      </button>
     </div>
   );
 }
