6e65dcdadbf28b7c4f2a0e093c2dd4c40e6fc03a
joblanco <jo_blanco9@yahoo.com.br>
2023-10-23 16:43:06 -0300

    Criação da page create account, redux instalado

---
+++ frontend/src/redux/actions/index.ts
@@ -0,0 +1,14 @@
+import type ILocations from '../../interfaces/Locations';
+
+interface Account {
+  name: string
+  surname: string
+  email: string
+  password: string
+  state: string
+  region: string
+}
+
+export const actionAccountCreation = (account: Account) => ({ type: 'CREATE_ACCOUNT', payload: account });
+
+export const actionFillStates = (states: ILocations[]) => ({ type: 'FILL_STATES_API', payload: states });
