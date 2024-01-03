099ff360030ac657b0302aeb35a89fd2ca355fb4
joblanco <jo_blanco9@yahoo.com.br>
2024-01-03 17:19:02 -0300

    Adicionado algumas regras de negócio, onChangeInput melhorado

--- frontend/src/pages/CreateAccount.tsx
+++ frontend/src/pages/CreateAccount.tsx
@@ -6,36 +6,30 @@ import type ILocations from '../interfaces/Locations';
 import type IGlobalState from '../interfaces/GlobalState';
 
 function CreateAccount() {
-  const [account, setAccount] = useState({ name: '', surname: '', email: '', state: 'Acre' });
+  const [account, setAccount] = useState({ name: '', surname: '', email: '', password: '', repeatedPassword: '', state: 'Acre' });
+  const [buttonToggle, setButtonToggle] = useState(true);
+  // Redux
   const globalState: IGlobalState = useSelector((state) => state) as IGlobalState;
   const dispatch = useDispatch();
+  // Validações
   const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+  const specialCharsRegex = /[`!@#$%^&*()_+=[\]{};':"\\|,.<>/?~]/;
 
   const onChangeInput = ({ name, value }: { name: string, value: string }): void => {
-    console.log(value);
-    console.log(name);
-    switch (name) {
-      case 'name':
-        setAccount({ ...account, name: value });
-        break;
-      case 'surname':
-        setAccount({ ...account, surname: value });
-        break;
-      case 'new-email':
-        setAccount({ ...account, email: value });
-        break;
-      case 'state':
-        setAccount({ ...account, state: value });
-        break;
-      default:
-        break;
-    }
+    setAccount({ ...account, [name]: value });
+    // const filledInputs = account.name.length > 0 && account.surname.length > 0 && account.email.length > 0 && account.password.length > 0 && account.repeatedPassword.length > 0 && account.state.length > 0;
+    const filledInputs = Object.keys(account).every(key => account[key].length > 0);
+    setButtonToggle(!filledInputs);
   };
 
   const submitAccount = (): void => {
-    const isEmailValid = emailRegex.test(account.email);
+    const isEmailValid: boolean = emailRegex.test(account.email);
+    if (account.name.length < 3) throw new Error('Nome muito pequeno');
+    if (account.surname.length < 3) throw new Error('Sobrenome muito pequeno');
     if (!isEmailValid) throw new Error('Email inválido');
-    let region = globalState.locationsApi.states.find((state) => state.nome === account.state)?.regiao.nome;
+    const validPass: boolean = account.password === account.repeatedPassword && account.password.length >= 8 && specialCharsRegex.test(account.password);
+    if (!validPass) throw new Error('Senha inválida');
+    let region: string | undefined = globalState.locationsApi.states.find((state) => state.nome === account.state)?.regiao.nome;
     if (region === undefined) region = '';
     dispatch(actionAccountCreation({
       name: account.name,
@@ -75,8 +69,8 @@ function CreateAccount() {
       <label htmlFor="email">E-mail:</label>
       <input
         type="email"
-        name="new-email"
-        id="new-email"
+        name="email"
+        id="email"
         onChange={({ target }) => { onChangeInput(target); }}
         value={account.email} />
 
@@ -84,20 +78,22 @@ function CreateAccount() {
       <input
         type="password"
         name="password"
-        id="password" />
+        id="password"
+        onChange={({ target }) => { onChangeInput(target); }} />
 
       <label htmlFor="repeat-password">Repita a sua senha:</label>
       <input
         type="password"
-        name="repeat-password"
-        id="repeat-password" />
+        name="repeatedPassword"
+        id="repeatedPassword"
+        onChange={({ target }) => { onChangeInput(target); }} />
 
       <select name="state" id="state" onChange={({ target }) => { onChangeInput(target); }} value={account.state}>
         {globalState.locationsApi.states.map((state: ILocations, index: number) => (
           <option key={index} value={state.nome}>{state.nome}</option>))}
       </select>
 
-      <input type="button" name="" id="" value="Enviar" onClick={submitAccount} />
+      <input type="button" name="submit-button" id="submit-button" value="Enviar" disabled={buttonToggle} onClick={submitAccount} />
     </div>
   );
 }
